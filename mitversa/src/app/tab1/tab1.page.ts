0b064640da4a87292a0c59ab96c3b0bf1b155510
Patricio <patricio62014@gmail.com>
2024-09-09 22:28:43 -0300

    Pagina de Register y de Repartidores

--- mitversa/src/app/tab1/tab1.page.ts
+++ mitversa/src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
+  selector: 'app-login',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
 
   constructor() {}
 
+  handleLogin() {
+    console.log('Login attempted with:', {
+      email: this.email,
+      password: this.password,
+      rememberMe: this.rememberMe,
+    });
+    // Implement login logic here
+  }
 }
