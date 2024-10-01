47727890ebb37a8285236b4e2af1314fb49993a9
Christian <82286848+Ozauri0@users.noreply.github.com>
2024-10-01 05:54:21 -0300

    Changes in user-home

--- mitversa/src/environments/environment.prod.ts
+++ mitversa/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ef63-181-161-168-44.ngrok-free.app' // siempre utilizar el http o https según corresponda
+  apiUrl: 'http://localhost:8000/' // siempre utilizar el http o https según corresponda
 };
